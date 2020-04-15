e31b9094ce252708198e0d2c3f00f6d1589ffbc0
Ivan Rodrigues <ivanrodfre@gmail.com>
2020-04-15 19:52:23 -0300

    Dev erro teste

--- Startup.cs
+++ Startup.cs
@@ -67,10 +67,10 @@ namespace Shop
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
